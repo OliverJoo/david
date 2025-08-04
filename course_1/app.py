7fc8042018c9c8cf40698bccfbc2aa035f4898f8
OliverJoo <jjaemoon@nate.com>
2025-08-04 13:11:40 +0900

    test2 데코레이터와 함수 코드를 삭제 - 기획 변경으로 해당 테스트 코드 삭제

--- course_1/app.py
+++ course_1/app.py
@@ -22,10 +22,6 @@ def menu():
 @app.route("/test1")
 def test1():
     return render_template('test1.html')
-
-@app.route("/test2")
-def test2():
-    return render_template('test2.html')
     
 if __name__ == "__main__":
     app.run(debug=True, port=5001) 
