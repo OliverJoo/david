f7583ca4a8f5cd38cdba0802c20bfb1f800450b3
OliverJoo <jjaemoon@nate.com>
2025-09-08 08:39:06 +0900

    feat: add pprint usage for debugging during PCCP test

---
+++ prep_PCCP_python/study/pprint.py
@@ -0,0 +1,10 @@
+import pprint
+
+def solution(numlist, n):
+    answer = [3, 4]
+    h = 'a'
+    return pprint.pprint(locals()) # nice for debugging
+
+
+if __name__== '__main__':
+    print(solution([4,3,345,2], 30))
